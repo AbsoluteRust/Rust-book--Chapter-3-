7f7a51e7f8e5788d4379600cbc97b7b2c16c7d15
AbsoluteRust <carrollchristopher00@gmail.com>
2022-03-12 12:57:42 +0000

    Rust book (Chapter 3)

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; //constants can be thought of as an "alias"
+
+
+
+}
